6a6aab5d40d977941d4662b54ac2d882791179e6
jessica estrella <jess.m.estrella@gmail.com>
2025-05-08 16:43:18 +0100

    Step 1-2 from Watch List Challenge

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,34 @@
+class MoviesController < ApplicationController
+
+  def index
+    @movie = @movie
+    
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def show
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+end
+
+# index, new, create, show, edit, update, and destroy
